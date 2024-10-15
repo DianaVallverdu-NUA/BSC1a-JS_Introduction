047848c6c2210b87718a3457a08b4714746454d7
Diana0604 <diana.vallverdu@gmail.com>
2024-10-15 10:37:43 +0100

    convert character constants to arrays

--- js/script.js
+++ js/script.js
@@ -1,17 +1,11 @@
 // define sources for each character choice
-const image1 = "assets/chikorita.png";
-const image2 = "assets/cyndaquil.png";
-const image3 = "assets/totodile.png";
+const imageSources = ["assets/chikorita.png", "assets/cyndaquil.png", "assets/totodile.png"];
 
 // define alternative text for each character choice
-const altText1 = "Initial Pokemon Chikorita";
-const altText2 = "Initial Pokemon Cyndaquil";
-const altText3 = "Initial Pokemon Totodile";
+const altText = ["Initial Pokemon Chikorita", "Initial Pokemon Cyndaquil", "Initial Pokemon Totodile"];
 
 // define text for paragraph for each character choice
-const characterName1 = "Chikorita";
-const characterName2 = "Cyndaquil";
-const characterName3 = "Totodile";
+const characterNames = ["Chikorita", "Cyndaquil", "Totodile"];
 
 // define HTML Element constants
 const myButton = document.getElementById("my-button");
@@ -27,7 +21,7 @@ let characterCounter = 1;
  * @returns when character is chosen
  */
 function clickFunction() {
-  console.log("============== NEW TESTING STARTED ==============");
+  console.log("============== NEW CLICK  ==============");
 
   // update character counter
   characterCounter = characterCounter + 1;
@@ -37,35 +31,15 @@ function clickFunction() {
     characterCounter = 1;
   }
 
+  console.log('character counter', characterCounter);
+
   // update paragraph text with new character
   myParagraph.innerHTML = "I am on character " + characterCounter;
 
   // find image related to specific character counter
-  console.log("Testing if character counter is one");
-  if (characterCounter === 1) {
-    characterImage.src = image1;
-    characterImage.alt = altText1;
-    myParagraph.innerHTML = characterName1;
-    return;
-  }
-
-  console.log("Testing if character counter is two");
-  if (characterCounter === 2) {
-    characterImage.src = image2;
-    characterImage.alt = altText2;
-    myParagraph.innerHTML = characterName2;
-    return;
-  }
-
-  console.log("Testing if character counter is three");
-  if (characterCounter === 3) {
-    characterImage.src = image3;
-    characterImage.alt = altText3;
-    myParagraph.innerHTML = characterName3;
-    return;
-  }
-
-  console.log("No more testing to do");
+  characterImage.src = imageSources[characterCounter - 1];
+  characterImage.alt = altText[characterCounter - 1];
+  myParagraph.innerHTML = characterNames[characterCounter - 1];
 }
 
 myButton.onclick = clickFunction;
