f77dd95184c426be034627afd5dc0ba63fb62588
Diana0604 <diana.vallverdu@gmail.com>
2024-10-15 16:12:20 +0100

    add comments to dragdrop.js

--- js/dragdrop.js
+++ js/dragdrop.js
@@ -1,36 +1,64 @@
+// store HTML elements of class complement in complements
 const complements = document.getElementsByClassName("complement");
+
+// drop zone is the area where complements can be dropped
 const dropZone = document.getElementById("character-img");
 
-function onDrop(event) {
-    // bring fancy hat to the position (clientX, clientY)
-    draggedImage.style.left = event.clientX - offsetX + "px";
-    draggedImage.style.top = event.clientY - offsetY + "px";
-    console.log("Element has been dropped");
-}
-
-function onDragOver(event) {
-    event.preventDefault();
-    console.log("Something is being dragged over me!");
-}
-
+//offsetX and offsetY store the initial offset from clicking on the dragged image
 let offsetX = 0;
 let offsetY = 0;
+
+//draggedImage stores the currently dragged image
 let draggedImage = undefined;
 
-function onDragStart(event) {
-    draggedImage = event.target;
-
-    const style = window.getComputedStyle(draggedImage, null); 
-
-    offsetX = event.clientX - parseInt(style.left);
-    offsetY = event.clientY - parseInt(style.top);
-    console.log("I'm being dragged")
+/**
+ * when something is dropped in the dropzone, update draggedImage style accordingly
+ * @param {Event} event stores information related to the current drop event
+ */
+function onDrop(event) {
+    //update draggedImage style to:
+    // event.clientX (horizontal mouse position) - offsetX (calculated initial offset)
+    // event.clientY (vertical mouse position) - offsetY (calculated initial offset)
+    draggedImage.style.left = event.clientX - offsetX + "px";
+    draggedImage.style.top = event.clientY - offsetY + "px";
 }
 
+/**
+ * prevent the event default to allow dropping to happen
+ * @param {Event} event stores information related to the current dragover event
+ */
+function onDragOver(event) {
+    event.preventDefault();
+}
+
+/**
+ * store draggedImage as the event target and compute initial offsetX and offsetY
+ * @param {Event} event stores information related to the current dragover event
+ */
+function onDragStart(event) {
+    //event.target points to the HTML Element that started the drag event, i.e.
+    //the currently dragged image
+    draggedImage = event.target;
+
+    //window.getComputedStyle returns the style of the element removing the CSS text
+    //for ex. "100px" becomes "100"
+    const style = window.getComputedStyle(draggedImage); 
+
+    //offset is calculated as:
+    //clientX (horizontal mouse position) - style.left (left position of the image)
+    //clientY (vertical mouse position) - style.top (left position of the image)
+    //parseInt is needed to transform the style strings to numbers
+    offsetX = event.clientX - parseInt(style.left);
+    offsetY = event.clientY - parseInt(style.top);
+}
+
+//link dropZone related events to dropZone object
 dropZone.ondrop = onDrop;
 dropZone.ondragover = onDragOver;
 
+//loop through HTML Elements stored inside the complements array
 for(let complement of complements) {
+    //link complement related events to each individual complement
     complement.ondragstart = onDragStart;
     complement.ondragover = onDragOver;
     complement.ondrop = onDrop;
